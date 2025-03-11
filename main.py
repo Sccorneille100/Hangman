e8e0bb8bdb44b19cfb29a1e66d6dd4f064118c3b
Sylvestre Corneille <127679758+Sccorneille100@users.noreply.github.com>
2025-03-11 16:46:49 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+best_num = "same"
+
+
+underscore_list = len(best_num) * ['_']
+
+
+def check(input, name):
+ if input in name:
+   print ("you are right so far")
+ else:
+  print("this is wrong");
+  return
+  main()
+
+def winlose():
+ pass
+
+
+def main ():
+ input = 'p'
+ print (underscore_list)
+ check(input, best_num)
+
+
+main()
+
+
